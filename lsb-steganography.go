cc5f75f1ed0ef881d766f5f9bdbbbf4fe21bd65d
Jamie Hands <jamie_hands0@protonmail.com>
2025-09-18 16:06:20 +0100

    feat: added basic file opening and defered closing

---
+++ lsb-steganography.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"bufio"
+	"image"
+	"image/png"
+	"strings"
+)
+
+func main() {
+	var err error
+
+	fmt.Println("Absolute image path: ")
+	reader := bufio.NewReader()
+	path, err := reader.ReadString()
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	if strings.Contains(path, ".png") {
+		image.RegisterFormat("png", "png", png.Decode(), png.DecodeConfig())
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+}
